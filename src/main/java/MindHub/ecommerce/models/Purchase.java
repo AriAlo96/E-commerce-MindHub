527feb15f20fd8627673d58818a83408368d03a3
Isaac <ifa24991@gmail.com>
2023-11-22 01:12:29 -0300

    Packages y primeras clases

---
+++ src/main/java/MindHub/ecommerce/models/Purchase.java
@@ -0,0 +1,19 @@
+package MindHub.ecommerce.models;
+
+import javax.persistence.ManyToOne;
+
+public class Purchase {
+    private Long id;
+    @ManyToOne
+    private Long clientId;
+    private Double totalPurchases;
+
+    public Purchase() {
+    }
+
+    public Purchase(Long clientId, Double totalPurchases) {
+        this.clientId = clientId;
+        this.totalPurchases = totalPurchases;
+    }
+
+}
