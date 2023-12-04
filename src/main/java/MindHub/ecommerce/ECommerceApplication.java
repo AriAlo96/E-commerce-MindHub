929b185f3f720eaa6999997abcce156424eb96cd
Sapio Luciano <sapioluciano99@gmail.com>
2023-12-04 17:14:29 -0300

    ultimo push antes del video

--- src/main/java/MindHub/ecommerce/ECommerceApplication.java
+++ src/main/java/MindHub/ecommerce/ECommerceApplication.java
@@ -30,11 +30,7 @@ public class ECommerceApplication {
                                       CreamRepository creamRepository)
     {
         return args -> {
-        Flavoring flavoring1 = new Flavoring("Pink and violet air freshener","Scent of roses and violets. Ideal for perfuming every corner of your home.",500,15.99,9,AMBIENT,"https://res.cloudinary.com/dndulhlws/image/upload/v1701276850/aromatizante-rosa-y-violeta_zduwqa.png",true);
-        Flavoring flavoring2 = new Flavoring("Strawberry and plum air freshener","A fruity essence that seduces your senses and transmits the freshness of summer.",500,14.99,4,AMBIENT,"https://res.cloudinary.com/dndulhlws/image/upload/v1701691772/aromatizante-frutilla-y-ciruela_ayyipu.png", true);
-        Flavoring flavoring3 = new Flavoring("Fabric air freshener caress of freshness","Energy and freshness between your sheets and pillows to start the day with the best aromas.",500,16.99,7,FABRICS,"https://res.cloudinary.com/dndulhlws/image/upload/v1701691760/aromatizante-cama-caricia-de-frescura_t81iuq.png", true);
-        Flavoring flavoring4 = new Flavoring("Linen air freshener","An unmistakable fragrance of clean clothes that permeates all your environments.",500,14.99,6,AMBIENT,"https://res.cloudinary.com/dndulhlws/image/upload/v1701691764/aromatizante-lino_bvxhq8.png", true);
-        
+
         };
     }
 }
